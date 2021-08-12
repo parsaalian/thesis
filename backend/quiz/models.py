3d64a58e8b8085ace2210541db104eefd61750b1
Parsa Alian <parsaalian0@gmail.com>
2021-08-12 13:05:50 +0430

    crawled investopedia definition data with selenium

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -6,6 +6,13 @@ class Pattern(models.Model):
     pattern_description = models.TextField()
 
 
+class PatternArticle(models.Model):
+    href = models.CharField(max_length=250, primary_key=True)
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    body = models.TextField()
+
+
 class PatternReference(models.Model):
     Referrer = models.ForeignKey(Pattern, on_delete=models.CASCADE)
     Referee = models.ForeignKey(Pattern, on_delete=models.CASCADE)
