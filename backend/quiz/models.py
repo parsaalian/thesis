57460430045c0860215dc85ac7ffcebc6b08b139
Parsa Alian <parsaalian0@gmail.com>
2021-08-12 11:06:14 +0430

    pattern models

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Pattern(models.Model):
+    pattern_key = models.CharField(max_length=250, primary_key=True)
+    pattern_description = models.TextField()
+
+
+class PatternReference(models.Model):
+    Referrer = models.ForeignKey(Pattern, on_delete=models.CASCADE)
+    Referee = models.ForeignKey(Pattern, on_delete=models.CASCADE)
